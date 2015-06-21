84492b43b2863f9667585694d743a039811bd7b2
H2CO3 <h2co3@h2co3.org>
2015-06-21 19:54:30 +0200

    do not indent namespaces

--- init.el
+++ init.el
@@ -239,6 +239,9 @@ With argument ARG, do this that many times."
  '(ido-first-match ((t (:inherit font-lock-keyword-face :weight bold))))
  '(ido-incomplete-regexp ((t (:inherit font-lock-default-face)))))
 
+;; Do not indent namespaces
+(c-set-offset 'innamespace 0)
+
 ;; Fix wrong C++11 lambda-as-argument indentation
 (defadvice c-lineup-arglist (around my activate)
   "Improve indentation of continued C++11 lambda function opened as argument."
